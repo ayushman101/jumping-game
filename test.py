8e6ef2a751a69269222351effc4056fb8ed5dd5f
ayushman101 <87219658+ayushman101@users.noreply.github.com>
2022-04-23 11:48:27 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -9,19 +9,19 @@ clock=pygame.time.Clock()    #get clock
 
 pygame.display.set_caption("Poseidons Dome")   #set window name
 
-screen=pygame.display.set_mode(WINDOW_SIZE)    #create window
+screen=pygame.display.set_mode(WINDOW_SIZE)    #create window with window size 
 
-img=pygame.image.load("IMG_20211122_165544.jpg")
+img=pygame.image.load("IMG_20211122_165544.jpg")  #get an image
 
-img_location=(0,0)
+img_location=(0,0)    # set image location variable
 
-screen.blit(img,img_location)
+screen.blit(img,img_location)   # add image on the window screen at image location
 
-while True:
-    for event in pygame.event.get():
-        if event.type==QUIT:
-            pygame.quit()
-            sys.exit()
+while True:  # game loop 
+    for event in pygame.event.get():  # event loop 
+        if event.type==QUIT:    # if window cross is clicked
+            pygame.quit()    # quit pygame
+            sys.exit()       # end the script
     
-    pygame.display.update()
-    clock.tick(60)
+    pygame.display.update()   #update window
+    clock.tick(60)   # fps controller 
