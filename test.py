3cc21ebeaadb016ec11c59eafd355f3ff8c67fbe
ayushman101 <87219658+ayushman101@users.noreply.github.com>
2022-04-26 10:15:53 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -13,6 +13,12 @@ screen=pygame.display.set_mode(WINDOW_SIZE)    #create window with window size
 
 img=pygame.image.load("6b7e7e672f4a3c82b564aaf6850f6579-boy-with-boombox-pixel-art.png")  #get an image
 
+text=pygame.font.Font(None,50)
+
+text_surface=text.render('My Game', False, 'White')
+
+
+
 img_location=[0,0]    # set image location variable
 
 move_right=False
@@ -24,12 +30,19 @@ while True:  # game loop
     
     screen.blit(img,img_location)   # add image on the window screen at image location
 
+    screen.blit(text_surface,(500,50))
+
     if move_left==True:   # change x point of location
         img_location[0]-=4   
     if move_right==True:    #change y point of location
         img_location[0]+=4
 
     
+    if img_location[0] > 1300:
+        img_location[0]=-200
+    if img_location[0]<-200:
+        img_location[0]=1280
+   
     for event in pygame.event.get():  # event loop 
         if event.type==QUIT:    # if window cross is clicked
             pygame.quit()    # quit pygame
@@ -46,6 +59,7 @@ while True:  # game loop
                 move_left=False
             if event.key==K_RIGHT:  # if button was right arrow 
                 move_right=False
+
         
     
     pygame.display.update()   #update window
